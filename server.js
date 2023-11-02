ae1cf039a05c0bd8c04b150bf375ed0d6324504d
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-02 14:40:38 +0530

    refresh leaderboard

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-const leaderboard = [];
+let leaderboard = [];
 const users = ["theyashwanthsai", "xzist23", "varunmuthannaka", "yuvftw"];
 const problems = [
-  { Name: "Poor Pigs", Difficulty: "Hard" },
-  { Name: "Minimum Sum of Mountain Triplets II", Difficulty: "Medium"},
-  { Name: "Convert Sorted Array to Binary Search Tree", Difficulty: "Easy"}
+  {
+    Name: "Find Mode in Binary Search Tree",
+    Difficulty: "Easy"
+  }
 ];
 
 const difficultyPoints = {
@@ -21,38 +22,45 @@ const difficultyPoints = {
   Hard: 3,
 };
 
+
 const getSub = async (req, res) => {
-  // const query = recentSubmissionsQuery;
-  // let limit = 3;
-  for (let i = 0; i < users.length; i++) {
-    const jdata = await getSubmissions(users[i]);
-    const solvedProblems = jdata.data.recentAcSubmissionList.map(submission => submission.title);
-
-    const userScore = solvedProblems.reduce((totalScore, problemTitle) => {
-        const problem = problems.find(p => p.Name === problemTitle);
-        if (problem) {
-        totalScore += difficultyPoints[problem.Difficulty];
-      }
-        return totalScore;
-    }, 0);
-
-    const userTime = jdata.data.recentAcSubmissionList[0].timestamp;
-
-    leaderboard.push({ username: users[i], score: userScore, time: userTime });
-  }
-
-    leaderboard.sort((a, b) => {
-    if (a.score === b.score) {
-        return b.time - a.time;
+    for (let i = 0; i < users.length; i++) {
+      const jdata = await getSubmissions(users[i]);
+      const solvedProblems = jdata.data.recentAcSubmissionList.map(submission => submission.title);
+  
+      const userScore = solvedProblems.reduce((totalScore, problemTitle) => {
+          const problem = problems.find(p => p.Name === problemTitle);
+          if (problem) {
+          totalScore += difficultyPoints[problem.Difficulty];
+        }
+          return totalScore;
+      }, 0);
+  
+      const userTime = jdata.data.recentAcSubmissionList[0].timestamp;
+  
+      leaderboard.push({ username: users[i], score: userScore, time: userTime });
     }
-    return b.score - a.score;
-    });
-    res.json(leaderboard);
+      
+      leaderboard.sort((a, b) => {
+      if (a.score === b.score) {
+          return b.time - a.time;
+      }
+      return b.score - a.score;
+      });
+      console.log(leaderboard);
+      // res.json(leaderboard);
+      leaderboard = []
+
 }
 
-app.get("/start", getSub)
+const leaderboardUpdateInterval = 10000;
+
+setInterval(getSub, leaderboardUpdateInterval);
 
 
+app.get("/start", (req, res) => {
+  res.json(leaderboard);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is started at : ${PORT}`);
