70f0ab744b6c6743d6f0ffd45a935665db88d7bd
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-10 22:59:08 +0530

    app.js

--- app.js
+++ app.js
@@ -1,118 +1,132 @@
-const socket = io();
-    let room = null;
-    let userName = "No";
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+// const api = require('leetapi');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const getSubmissions = require('./api/api')
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
-    const roomForm = document.getElementById('room-form');
-    const chat = document.getElementById('chat');
-    function getName(){
-        return prompt("Enter Username")
-    }
-    document.getElementById('join').addEventListener('click', () => {
-        const name = getName();
-        room = document.getElementById('room').value;
+const rooms = new Map();
+const users ={}
+const leaderboard = {};
+// const problems = {};
+io.on('connection', (socket) => {
 
-        socket.emit('join', { room, name });
-        roomForm.style.display = 'none';
-        chat.style.display = 'block';
-    });
+  socket.on('join', ({room, name}) => {
+    socket.join(room);
+    socket.room = room;
+    socket.name = name;
 
-    document.getElementById('create').addEventListener('click', () => {
-        const name = getName();
-        room = document.getElementById('room').value;
-
-        socket.emit('join', { room, name });
-
-        roomForm.style.display = 'none';
-        chat.style.display = 'block';
-        userName = name;
-        // console.log(userName);
-    });
-
-    document.getElementById('send').addEventListener('click', () => {
-        const message = document.getElementById('message').value;
-        // console.log(userName);
-        socket.emit('chat message', {message});
-        document.getElementById('message').value = '';
-    });
-
-
-    document.getElementById('ready').addEventListener('click', () => {
-        room = document.getElementById('room').value;
-        socket.emit('ready', room)
-    })
-
-
-    document.getElementById('start').addEventListener('click', () => {
-        room = document.getElementById('room').value;
-        socket.emit('contest', room)
-    })
-
-    document.getElementById('end').addEventListener('click', () => {
-        room = document.getElementById('room').value;
-        socket.emit('end', room)
-    })
-
-    socket.on('user joined', (name) => {
-
-        addMessage(`User ${name} joined the room.`);
-    });
-
-    socket.on('user left', (name) => {
-        addMessage(`User ${name} left the room.`);
-    });
-
-    socket.on('show chat message', ({name, message}) => {
-        addMessage(`${name}: ${message}`);
-    });
-
-
-
-    function addMessage(text) {
-        const li = document.createElement('li');
-        li.textContent = text;
-        document.getElementById('messages').appendChild(li);
+    if (!rooms.has(room)) {
+      rooms.set(room, []);
     }
 
-    socket.on('updateLeaderboard', (updatedLeaderboard) => {
+    io.to(room).emit('user joined', socket.name);
+  });
 
-        console.log('Updated Leaderboard:', updatedLeaderboard);
+  socket.on('chat message', ({message}) => {
+    const name = socket.name;
+    io.to(socket.room).emit('show chat message', {name, message});
+  });
 
-        addLeaderboard(updatedLeaderboard);
-        });
+  // takein user name for the room and append for that room.
+  socket.on('ready', (room) =>{
+    socket.room = room;
+    if (!users.hasOwnProperty(socket.room)) {
+      // If not, create an empty array for that key
+      users[socket.room] = [];
+    }
+    
+    // Check if the socket.name is not already in the array before pushing it
+    if (!users[socket.room].includes(socket.name)) {
+      users[socket.room].push(socket.name);
+    }    
+    // console.log(users);
+  });
 
-        
-      // Function to add leaderboard to the UI
-        function addLeaderboard(leaderboardData) {
+  socket.on('contest', (room) => {
+    socket.room = room;
+    // console.log(users[socket.room]);
 
-        const leaderboardElement = document.getElementById('leaderboard');
-            
-        // Clear previous leaderboard data
-        leaderboardElement.innerHTML = '';
-            
-        // Create a table to display the leaderboard
-        const table = document.createElement('table');
-        table.border = '1';
+    const problems = [
+      {
+        Name: "Find Mode in Binary Search Tree",
+        Difficulty: "Easy"
+      }
+    ];
 
-        const headerRow = table.insertRow();
-        const usernameHeader = headerRow.insertCell(0);
-        const scoreHeader = headerRow.insertCell(1);
-        // const timeHeader = headerRow.insertCell(2);
-        usernameHeader.innerHTML = '<b>Username</b>';
-        scoreHeader.innerHTML = '<b>Score</b>';
-        // timeHeader.innerHTML = '<b>Time</b>';
-            
-
-        leaderboardData.forEach((entry, index) => {
-            const row = table.insertRow();
-            const usernameCell = row.insertCell(0);
-            const scoreCell = row.insertCell(1);
-            // const timeCell = row.insertCell(2);
-            
-            usernameCell.innerHTML = entry.username;
-            scoreCell.innerHTML = entry.score;
-            // timeCell.innerHTML = entry.time;
-        });
-        
-        // Append the table to the leaderboard element
-        leaderboardElement.appendChild(table);
+    const difficultyPoints = {
+      Easy: 1,
+      Medium: 2,
+      Hard: 3,
+    };
+    
+    const getSub = async (req, res) => {
+      // Create an array to store user scores for the current room
+      let lb = [];
+    
+      for (let i = 0; i < users[socket.room].length; i++) {
+        const jdata = await getSubmissions(users[socket.room][i]);
+        const solvedProblems = jdata.data.recentAcSubmissionList.map(submission => submission.title);
+        const userScore = solvedProblems.reduce((totalScore, problemTitle) => {
+          const problem = problems.find(p => p.Name === problemTitle);
+          if (problem) {
+            totalScore += difficultyPoints[problem.Difficulty];
+          }
+          return totalScore;
+        }, 0);
+    
+        const userTime = jdata.data.recentAcSubmissionList[0].timestamp;
+    
+        lb.push({ username: users[socket.room][i], score: userScore, time: userTime });
+      }
+    
+      // Sort the leaderboard array
+      lb.sort((a, b) => {
+        if (a.score === b.score) {
+          return b.time - a.time;
         }
+        return b.score - a.score;
+      });
+    
+      // Store the leaderboard array in the global leaderboard object
+      leaderboard[socket.room] = lb;
+    
+      // Log the current leaderboard for the room
+      console.log(leaderboard[socket.room]);
+      io.to(socket.room).emit('updateLeaderboard', leaderboard[socket.room]);
+    };
+
+    // getSub();
+    const leaderboardUpdateInterval = 10000;
+
+    setInterval(getSub, leaderboardUpdateInterval);
+
+  });
+  socket.on('end', (room) => {
+    
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.room) {
+      const users = rooms.get(socket.room);
+      if (users) {
+        const index = users.indexOf(socket.id);
+        if (index !== -1) {
+          users.splice(index, 1);
+        }
+      }
+      io.to(socket.room).emit('user left', socket.name);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+
